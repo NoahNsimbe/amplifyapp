f05e0da2ace624aa556074e72357c23d6041e5a7
Noah Nsimbe <nsimbenoah@gmail.com>
2021-02-24 15:21:50 +0300

    machine

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const cities = ["Kampala", "Nairobi", "Lagos"];
+  console.log(...cities);
   return (
     <div className="App">
       <header className="App-header">
